b3c67e2f6abbd74754e27a6a1730acc3b31c7cbc
sarah <ssh2198@columbia.edu>
2023-07-04 20:36:55 -0500

    Created heatmap from genesets

--- app.R
+++ app.R
@@ -10,6 +10,9 @@
 library(shiny)
 library(tidyverse)
 library(devtools)
+library(gtools)
+library(data.table)
+library(ggplot2)
 
 install_github("hurlab/richR")
 library(richR)
@@ -20,6 +23,8 @@ gs3 <- read.delim("~/Desktop/Hur Lab/enrichment-analysis3/data/KEGG_HF36wk_vs_HF
 
 genesets <- list(gs1, gs2, gs3)
 
+
+# this function merges a list of genesets together to prepare for clustering
 cluster <- function(genesets) {
   
   # suffix all non 'Term' columns with their index
@@ -69,7 +74,69 @@ cluster <- function(genesets) {
 clustered_gs <- richCluster(x=merged_gs, minSize=3)
 write.table(clustered_gs, file='/Users/sarahhong/Desktop/Hur Lab/enrichment-analysis3/data/clustered_data.txt', sep='\t')
 
-clustered_data <- richCluster(x=combined_data, minSize=3)
+hmap_prepare <- function(clustered_gs, num_clusters) {
+  
+  # create tibble with Term and AnnotationCluster
+  x <- as_tibble(cbind(clustered_gs$Term, clustered_gs$AnnotationCluster))
+  colnames(x) <- c('Term', 'Cluster')
+  
+  # order clusters numerically
+  x <- x[order(as.numeric(x$Cluster)), ]
+  
+  clusters <- c()
+  #group the Padj values by cluster using the Annotation clustered levels
+  clustered_gs$AnnotationCluster <- as.factor(clustered_gs$AnnotationCluster)
+  for (i in 1:length(levels(clustered_gs$AnnotationCluster))){
+    clusters[[i]] <- clustered_gs[clustered_gs$AnnotationCluster == mixedsort(levels(clustered_gs$AnnotationCluster))[i],]
+  }
+  
+  # define Padj column names
+  padj_cols <- c()
+  for (i in seq_along(genesets)) {
+    padj_cols <- c(padj_cols, paste0('Padj', i))
+  }
+  
+  # subset Padj columns of clustered_gs into values
+  values <- data.frame()
+  values <- clustered_gs[, padj_cols]
+  values$p_rank <- rowMeans(values, na.rm=TRUE)
+  
+  # order clusters by p_rank
+  setorder(values, p_rank, na.last=TRUE)
+  values <- select(values, -p_rank)
+  
+  # combine Term and Cluster with Padj values
+  final_data <- cbind(x, values)
+  final_data <- final_data[c(1:num_clusters), ]
+  
+  return(final_data)
+}
+
+make_heatmap <- function(final_data) {
+  hmap <- final_data
+  hmap$Cluster <- sub("^", "Cluster", hmap$Cluster) # prefix with 'Cluster'
+  
+  # rename 'Padj' columns to 'GS'
+  new_cols <- c()
+  for (i in seq_along(genesets)) {
+    new_cols <- c(new_cols, paste0('GS', i))
+  }
+  colnames(hmap) <- c('Term', 'Cluster', new_cols)
+  
+  # melt the data for heatmap construction
+  melted_data <- melt(hmap)
+  
+  # plot the heatmap
+  p <- ggplot(melted_data, aes(variable, Term)) +
+    geom_tile(aes(fill=value), colour='white') +
+    labs(x='Genesets', y='Clusters', title='Adjusted p-value per cluster') +
+    scale_y_discrete(labels=melted_data$Cluster) +
+    scale_fill_gradient(low='red', high='green') +
+    theme(axis.text=element_text(size=12))
+  return(p)
+}
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
