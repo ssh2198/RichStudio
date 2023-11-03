dd2044a484b7e2dfc3df567bc646c7bcf0233992
Sarah Hong <ssh2198@columbia.edu>
2023-11-03 04:03:36 -0400

    update renv.lock

--- app.R
+++ app.R
@@ -30,7 +30,6 @@ library(bioAnno)
 
 # Set working directory with config.yml (Fix later)
 config_vars <- config::get("hurlab-server")
-print(config_vars$project_directory)
 setwd(config_vars$project_directory)
 
 options(shiny.error = browser)
