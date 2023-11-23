c0469417001e95a26a9ecf2c6de493f570d5625d
Sarah Hong <ssh2198@columbia.edu>
2023-11-23 02:13:30 -0500

    Fix homepage

--- home_tab.R
+++ home_tab.R
@@ -2,7 +2,7 @@
 homeTabUI <- function(id, tabName) {
   ns <- NS(id)
   tabItem(tabName = tabName,
-    h1("Welcome to RichStudio v0.1.0!"),
+    h1("Welcome to RichStudio v0.1.3!"),
     p("Upload from differential expression geneset (DEG) or enrichment result. Supports kappa clustering and multiple visualizations.")
   )
 }
